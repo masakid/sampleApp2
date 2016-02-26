e2f91e9bc10815ed54f3261413215fc50c453e4a
masakid <runmk827@gmail.com>
2016-02-26 23:32:58 +0900

    six

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -29,6 +29,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "name should not be too long" do
     @user.name = "a"*51
     assert_not @user.valid?
